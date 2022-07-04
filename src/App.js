534a13b5683d34bf9ba0a325f2f134bb3a77bef9
jayeshgit93@gmail.com <jayeshgit93@gmail.com>
2022-07-04 19:09:45 +0530

    06 - modal overlay with react portals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 function App() {
   return (
-    <div>
+    <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </div>
+    </Fragment>
   );
 }
 
